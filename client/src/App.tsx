3d0e29d0db40f9c09aff10bb9de55daf1b202535
nathanbaleeta <nbaleeta@gmail.com>
2025-11-04 22:21:48 +0300

    added client react pages

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import { Outlet } from 'react-router';
+import { ReactRouterAppProvider } from '@toolpad/core/react-router';
+import type { Navigation } from '@toolpad/core/AppProvider';
+
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+];
+
+const BRANDING = {
+  title: "Health Monitoring Tool",
+};
+
+
+export default function App() {
+  
+  return (
+    <ReactRouterAppProvider navigation={NAVIGATION} branding={BRANDING}>
+      <Outlet />
+    </ReactRouterAppProvider>
+  );
+}
